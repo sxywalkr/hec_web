5b15c03d133f129bf692bd9918947587839206cd
zyxgo <me.rahmat.hidayat@gmail.com>
2020-03-28 11:02:28 +0800

    aa

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -8,7 +8,9 @@ import * as ROLES from '../../constants/roles';
 
 import { Container, Menu } from 'semantic-ui-react';
 import { Offline, Online } from "react-detect-offline";
+import { detect } from 'detect-browser'
 
+const browser = detect()
 const Navigation = () => (
   <AuthUserContext.Consumer>
     {authUser =>
@@ -43,8 +45,8 @@ const NavigationNonAuth = () => (
       <Menu.Item name="display" as={Link} to={ROUTES.DISPLAY} />
       <Menu.Menu position="right">
         {/* <Menu.Item><Online>Connected from {window.navigator.userAgent}</Online></Menu.Item> */}
-        <Menu.Item><Online>Connected</Online></Menu.Item>
-        <Menu.Item><Offline>Disconnected</Offline></Menu.Item>
+        <Menu.Item><Online>Connected - {browser.name} {browser.version}</Online></Menu.Item>
+        <Menu.Item><Offline>Disconnected - {browser.name} {browser.version}</Offline></Menu.Item>
         <Menu.Item name="signin" as={Link} to={ROUTES.SIGN_IN} />
       </Menu.Menu>
     </Container>
